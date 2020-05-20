10c8237da71606d71e9c2577856a224103a82d48
Valter Lafuente Junior <valterlafuente@yahoo.com.br>
2020-05-20 15:15:57 -0300

    implementação da classe ProdutoDao e BuscarProdutoServlet.

---
+++ src/main/java/com/poo/servlet/BuscarProdutoServlet.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.poo.servlet;
+
+import com.poo.dao.ProdutoDao;
+import com.poo.model.Produto;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Valter Lafuente Junior
+ */
+@WebServlet(name = "buscarProdutoServlet", urlPatterns = {"/buscarProdutoServlet"})
+public class BuscarProdutoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       	
+        @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	try {
+	    List<Produto> listaProduto = ProdutoDao.buscar(request.getParameter("Busca"));
+	    request.setAttribute("listaProduto", listaProduto);	
+			
+	} catch (Exception e) {
+	   request.setAttribute("mensagem", "Erro de banco de dados: " + e.getMessage());
+			
+	}
+	   RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/paginas/buscaProduto.jsp");
+  	   dispatcher.forward(request, response);
+	}
+} 
+		
