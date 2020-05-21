d22fd357c92a7c7052362989ea2dfb11a03e05ad
Valter Lafuente Junior <valterlafuente@yahoo.com.br>
2020-05-21 17:06:41 -0300

    Implementação da classe  GerenciarFuncionario servlet e jsp

--- src/main/java/com/poo/servlet/GerenciarFuncionarioServlet.java
+++ src/main/java/com/poo/servlet/GerenciarFuncionarioServlet.java
@@ -19,39 +19,37 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Valter Lafuente Junior
  */
-@WebServlet(name = "GerenciarFuncionarioServlet", urlPatterns = {"/gerenciarFuncionarioServlet"})
+@WebServlet(name = "gerenciarFuncionarioServlet", urlPatterns = {"/gerenciarFuncionarioServlet"})
 public class GerenciarFuncionarioServlet extends HttpServlet {
 
-private final FuncionarioDao funcionarioDao = new FuncionarioDao(); 
-	private static final long serialVersionUID = 1L;
-       
-    
+    private final FuncionarioDao funcionarioDao = new FuncionarioDao();
+    private static final long serialVersionUID = 1L;
+
     public GerenciarFuncionarioServlet() {
         super();
     }
 
-	
-@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String acao = request.getParameter("acao"); 
-		String id = request.getParameter("id");
-		try {
-			if(acao != null && acao.equals("Excluir")) {  
-				Integer cod = Integer.parseInt(id);
-				funcionarioDao.excluir(cod);
-				request.setAttribute("mensagem", "Funcionario Excluido com sucesso!!");
-			}
-			request.setAttribute("funcionarios", funcionarioDao.getFuncionario1());
-			
-		} catch (SQLException e) {
-			request.setAttribute("mensagem","Erro de banco de dados");
-			
-		} catch (ClassNotFoundException e) {
-			
-			request.setAttribute("mensagem", "Erro de driver");
-		}
-	
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/gerenciarFuncionario.jsp");
-	    dispatcher.forward(request, response);
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String acao = request.getParameter("acao");
+        String id = request.getParameter("id");
+        try {
+            if (acao != null && acao.equals("Excluir")) {
+                Integer cod = Integer.parseInt(id);
+                funcionarioDao.excluir(cod);
+                request.setAttribute("mensagem", "Funcionario Excluido com sucesso!!");
+            }
+            request.setAttribute("funcionarios", funcionarioDao.getFuncionario1());
+
+        } catch (SQLException e) {
+            request.setAttribute("mensagem", "Erro de banco de dados");
+
+        } catch (ClassNotFoundException e) {
+
+            request.setAttribute("mensagem", "Erro de driver");
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/GerenciarFuncionario.jsp");
+        dispatcher.forward(request, response);
+    }
 }
